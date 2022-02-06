a1ebd465180bfa52d1b326c927462ebc03095342
issiscv <issiscv46@gmail.com>
2022-02-07 01:57:40 +0900

    FEAT: @createdBy, @lastModifiedBy 를 위한 AwareAuditConfig 개발

---
+++ src/main/java/couch/camping/config/AwareAuditConfig.java
@@ -0,0 +1,22 @@
+package couch.camping.config;
+
+import couch.camping.domain.member.entity.Member;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+public class AwareAuditConfig implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Member member = (Member) authentication.getPrincipal();
+        return Optional.of(member.getNickname());
+    }
+}
