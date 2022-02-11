33d25b11377ce7239ecb31c6f5298fc9b6104076
Longstick0 <789456jang@naver.com>
2022-02-12 02:20:48 +0900

    FIX: 캠핑장 조건 다중 조회 코드 수정
    캠핑장 데이터를 app계층으로 불러와 적절히 파싱해서 클라이언트로 넘겨주던 점을
    Querydsl을 사용해서 DB에서 불러올 때 데이터를 가공해줌

--- src/main/java/couch/camping/controller/camp/dto/response/CampSearchPagingResponseDto.java
+++ src/main/java/couch/camping/controller/camp/dto/response/CampSearchPagingResponseDto.java
@@ -1,2 +1,19 @@
-package couch.camping.controller.camp.dto.response;public class CampSearchPagingResponseDto {
+package couch.camping.controller.camp.dto.response;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.*;
+
+import java.util.List;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CampSearchPagingResponseDto {
+
+    private int totalPages;
+
+    private int totalElements;
+
+    List<CampSearchResponseDto> contents;
 }
