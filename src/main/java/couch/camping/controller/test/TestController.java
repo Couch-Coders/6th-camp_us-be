e7f36182e03b04d1da823ac9ff606c110c037f15
Longstick0 <789456jang@naver.com>
2022-02-23 03:07:52 +0900

    TEST: CICD 테스트

--- src/main/java/couch/camping/controller/test/TestController.java
+++ src/main/java/couch/camping/controller/test/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    //TEST
     @GetMapping("/test")
     public String test() {
         return "ok";
