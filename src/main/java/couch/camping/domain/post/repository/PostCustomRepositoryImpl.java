b37fa09144f019c758be1150bea95d4ef14a05d4
issiscv <issiscv46@gmail.com>
2022-03-16 10:34:01 +0900

    REFACTOR: 게시글 조회 쿼리 회원과 페치 조인 후 조회

--- src/main/java/couch/camping/domain/post/repository/PostCustomRepositoryImpl.java
+++ src/main/java/couch/camping/domain/post/repository/PostCustomRepositoryImpl.java
@@ -3,13 +3,16 @@ package couch.camping.domain.post.repository;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import couch.camping.domain.post.entity.Post;
+import couch.camping.domain.post.entity.QPost;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
+import static couch.camping.domain.member.entity.QMember.member;
 import static couch.camping.domain.post.entity.QPost.post;
 
 @RequiredArgsConstructor
@@ -18,11 +21,12 @@ public class PostCustomRepositoryImpl implements PostCustomRepository{
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Page<Post> findAllByIdWithPaging(String postType, Pageable pageable) {
+    public Page<Post> findAllByIdWithFetchJoinMemberPaging(String postType, Pageable pageable) {
         List<Post> content = queryFactory
                 .select(post)
                 .from(post)
                 .where(getEq(postType))
+                .join(post.member, member).fetchJoin()
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .orderBy(post.createdDate.desc())
@@ -61,5 +65,17 @@ public class PostCustomRepositoryImpl implements PostCustomRepository{
                 .fetchOne();
 
         return new PageImpl<>(content, pageable, total);
+    }
+
+    @Override
+    public Optional<Post> findByIdWithFetchJoinMember(Long postId) {
+        Post post = queryFactory
+                .select(QPost.post)
+                .from(QPost.post)
+                .join(QPost.post.member, member).fetchJoin()
+                .where(QPost.post.id.eq(postId))
+                .fetchOne();
+
+        return Optional.ofNullable(post);
     }
 }
