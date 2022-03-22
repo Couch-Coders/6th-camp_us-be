e72917b8ea9332ac042b929300babb2e646259f5
issiscv <issiscv46@gmail.com>
2022-03-22 17:27:12 +0900

    REFACTOR: 알림 전체 조회 API 응답 DTO 수정
    공통 : postId, 해당 게시글의 이미지 리스트
    게시글 댓글, 댓글 좋아요 알림 : 게시글 제목, 댓글 내용
    게시글 좋아요 : 게시글 제목

--- src/main/java/couch/camping/controller/member/dto/response/NotificationCommentWriteRetrieveResponseDto.java
+++ src/main/java/couch/camping/controller/member/dto/response/NotificationCommentWriteRetrieveResponseDto.java
@@ -1,11 +1,15 @@
 package couch.camping.controller.member.dto.response;
 
 import couch.camping.domain.notification.entity.Notification;
+import couch.camping.domain.postimage.entity.PostImage;
 import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,11 +19,21 @@ public class NotificationCommentWriteRetrieveResponseDto extends MemberNotificat
 
     private Long commentId;
 
+    private Long postId;
+
+    private String postTitle;
+
     private String content;
+
+    private List<String> postImgUrlList = new ArrayList<>();
 
     public NotificationCommentWriteRetrieveResponseDto(Notification notification) {
         super(notification, "commentWrite");
         this.commentId = notification.getWriteComment().getId();
         this.content = notification.getWriteComment().getContent();
+        this.postTitle = notification.getWriteComment().getPost().getTitle();
+        for (PostImage postImage : notification.getWriteComment().getPost().getPostImageList()) {
+            this.postImgUrlList.add(postImage.getImgUrl());
+        }
     }
 }
